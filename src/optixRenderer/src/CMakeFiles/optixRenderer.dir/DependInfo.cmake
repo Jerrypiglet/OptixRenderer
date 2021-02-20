2263fc55f1e244b6d96f5136be3e10af29c62887
jerrypiglet <jerrypiglet@gmail.com>
2021-02-20 15:25:30 -0800

    rui first commit

---
+++ src/optixRenderer/src/CMakeFiles/optixRenderer.dir/DependInfo.cmake
@@ -0,0 +1,57 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zhl/OptixRenderer/src/optixRenderer/src/creator/createAreaLight.cpp" "/home/zhl/OptixRenderer/src/optixRenderer/src/CMakeFiles/optixRenderer.dir/creator/createAreaLight.cpp.o"
+  "/home/zhl/OptixRenderer/src/optixRenderer/src/creator/createCamera.cpp" "/home/zhl/OptixRenderer/src/optixRenderer/src/CMakeFiles/optixRenderer.dir/creator/createCamera.cpp.o"
+  "/home/zhl/OptixRenderer/src/optixRenderer/src/creator/createContext.cpp" "/home/zhl/OptixRenderer/src/optixRenderer/src/CMakeFiles/optixRenderer.dir/creator/createContext.cpp.o"
+  "/home/zhl/OptixRenderer/src/optixRenderer/src/creator/createEnvmap.cpp" "/home/zhl/OptixRenderer/src/optixRenderer/src/CMakeFiles/optixRenderer.dir/creator/createEnvmap.cpp.o"
+  "/home/zhl/OptixRenderer/src/optixRenderer/src/creator/createGeometry.cpp" "/home/zhl/OptixRenderer/src/optixRenderer/src/CMakeFiles/optixRenderer.dir/creator/createGeometry.cpp.o"
+  "/home/zhl/OptixRenderer/src/optixRenderer/src/creator/createMaterial.cpp" "/home/zhl/OptixRenderer/src/optixRenderer/src/CMakeFiles/optixRenderer.dir/creator/createMaterial.cpp.o"
+  "/home/zhl/OptixRenderer/src/optixRenderer/src/creator/createPointFlashLight.cpp" "/home/zhl/OptixRenderer/src/optixRenderer/src/CMakeFiles/optixRenderer.dir/creator/createPointFlashLight.cpp.o"
+  "/home/zhl/OptixRenderer/src/optixRenderer/src/creator/createTextureSampler.cpp" "/home/zhl/OptixRenderer/src/optixRenderer/src/CMakeFiles/optixRenderer.dir/creator/createTextureSampler.cpp.o"
+  "/home/zhl/OptixRenderer/src/optixRenderer/src/inout/loadBsdfFromXML.cpp" "/home/zhl/OptixRenderer/src/optixRenderer/src/CMakeFiles/optixRenderer.dir/inout/loadBsdfFromXML.cpp.o"
+  "/home/zhl/OptixRenderer/src/optixRenderer/src/inout/loadLightFromXML.cpp" "/home/zhl/OptixRenderer/src/optixRenderer/src/CMakeFiles/optixRenderer.dir/inout/loadLightFromXML.cpp.o"
+  "/home/zhl/OptixRenderer/src/optixRenderer/src/inout/loadSensorFromXML.cpp" "/home/zhl/OptixRenderer/src/optixRenderer/src/CMakeFiles/optixRenderer.dir/inout/loadSensorFromXML.cpp.o"
+  "/home/zhl/OptixRenderer/src/optixRenderer/src/inout/loadShapeFromXML.cpp" "/home/zhl/OptixRenderer/src/optixRenderer/src/CMakeFiles/optixRenderer.dir/inout/loadShapeFromXML.cpp.o"
+  "/home/zhl/OptixRenderer/src/optixRenderer/src/inout/readXML.cpp" "/home/zhl/OptixRenderer/src/optixRenderer/src/CMakeFiles/optixRenderer.dir/inout/readXML.cpp.o"
+  "/home/zhl/OptixRenderer/src/optixRenderer/src/inout/relativePath.cpp" "/home/zhl/OptixRenderer/src/optixRenderer/src/CMakeFiles/optixRenderer.dir/inout/relativePath.cpp.o"
+  "/home/zhl/OptixRenderer/src/optixRenderer/src/inout/rgbe.cpp" "/home/zhl/OptixRenderer/src/optixRenderer/src/CMakeFiles/optixRenderer.dir/inout/rgbe.cpp.o"
+  "/home/zhl/OptixRenderer/src/optixRenderer/src/material/albedo.cpp" "/home/zhl/OptixRenderer/src/optixRenderer/src/CMakeFiles/optixRenderer.dir/material/albedo.cpp.o"
+  "/home/zhl/OptixRenderer/src/optixRenderer/src/material/depth.cpp" "/home/zhl/OptixRenderer/src/optixRenderer/src/CMakeFiles/optixRenderer.dir/material/depth.cpp.o"
+  "/home/zhl/OptixRenderer/src/optixRenderer/src/material/dielectric.cpp" "/home/zhl/OptixRenderer/src/optixRenderer/src/CMakeFiles/optixRenderer.dir/material/dielectric.cpp.o"
+  "/home/zhl/OptixRenderer/src/optixRenderer/src/material/diffuse.cpp" "/home/zhl/OptixRenderer/src/optixRenderer/src/CMakeFiles/optixRenderer.dir/material/diffuse.cpp.o"
+  "/home/zhl/OptixRenderer/src/optixRenderer/src/material/mask.cpp" "/home/zhl/OptixRenderer/src/optixRenderer/src/CMakeFiles/optixRenderer.dir/material/mask.cpp.o"
+  "/home/zhl/OptixRenderer/src/optixRenderer/src/material/metallic.cpp" "/home/zhl/OptixRenderer/src/optixRenderer/src/CMakeFiles/optixRenderer.dir/material/metallic.cpp.o"
+  "/home/zhl/OptixRenderer/src/optixRenderer/src/material/microfacet.cpp" "/home/zhl/OptixRenderer/src/optixRenderer/src/CMakeFiles/optixRenderer.dir/material/microfacet.cpp.o"
+  "/home/zhl/OptixRenderer/src/optixRenderer/src/material/normal.cpp" "/home/zhl/OptixRenderer/src/optixRenderer/src/CMakeFiles/optixRenderer.dir/material/normal.cpp.o"
+  "/home/zhl/OptixRenderer/src/optixRenderer/src/material/phong.cpp" "/home/zhl/OptixRenderer/src/optixRenderer/src/CMakeFiles/optixRenderer.dir/material/phong.cpp.o"
+  "/home/zhl/OptixRenderer/src/optixRenderer/src/material/roughness.cpp" "/home/zhl/OptixRenderer/src/optixRenderer/src/CMakeFiles/optixRenderer.dir/material/roughness.cpp.o"
+  "/home/zhl/OptixRenderer/src/optixRenderer/src/optixRenderer.cpp" "/home/zhl/OptixRenderer/src/optixRenderer/src/CMakeFiles/optixRenderer.dir/optixRenderer.cpp.o"
+  "/home/zhl/OptixRenderer/src/optixRenderer/src/postprocessing/filter.cpp" "/home/zhl/OptixRenderer/src/optixRenderer/src/CMakeFiles/optixRenderer.dir/postprocessing/filter.cpp.o"
+  "/home/zhl/OptixRenderer/src/optixRenderer/src/sampler/sampler.cpp" "/home/zhl/OptixRenderer/src/optixRenderer/src/CMakeFiles/optixRenderer.dir/sampler/sampler.cpp.o"
+  "/home/zhl/OptixRenderer/src/optixRenderer/src/utils/ptxPath.cpp" "/home/zhl/OptixRenderer/src/optixRenderer/src/CMakeFiles/optixRenderer.dir/utils/ptxPath.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/zhl/NVIDIA-OptiX-SDK-6.0.0-linux64/include"
+  "sutil"
+  "optixRenderer/include"
+  "."
+  "/home/zhl/NVIDIA-OptiX-SDK-6.0.0-linux64/include/optixu"
+  "/usr/local/cuda/include"
+  "device_include"
+  "/home/zhl/anaconda3/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/zhl/OptixRenderer/src/sutil/CMakeFiles/sutil_sdk.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/sutil/CMakeFiles/sutil_sdk.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zhl/OptixRenderer/src/sutil/Camera.cpp" "/home/zhl/OptixRenderer/src/sutil/CMakeFiles/sutil_sdk.dir/Camera.cpp.o"
+  "/home/zhl/OptixRenderer/src/sutil/OptiXMesh.cpp" "/home/zhl/OptixRenderer/src/sutil/CMakeFiles/sutil_sdk.dir/OptiXMesh.cpp.o"
+  "/home/zhl/OptixRenderer/src/sutil/sutil.cpp" "/home/zhl/OptixRenderer/src/sutil/CMakeFiles/sutil_sdk.dir/sutil.cpp.o"
+  "/home/zhl/OptixRenderer/src/sutil/tinyobjloader/objLoader.cpp" "/home/zhl/OptixRenderer/src/sutil/CMakeFiles/sutil_sdk.dir/tinyobjloader/objLoader.cpp.o"
+  "/home/zhl/OptixRenderer/src/sutil/tinyplyloader/plyLoader.cpp" "/home/zhl/OptixRenderer/src/sutil/CMakeFiles/sutil_sdk.dir/tinyplyloader/plyLoader.cpp.o"
+  "/home/zhl/OptixRenderer/src/sutil/tinyplyloader/tinyply.cpp" "/home/zhl/OptixRenderer/src/sutil/CMakeFiles/sutil_sdk.dir/tinyplyloader/tinyply.cpp.o"
+  "/home/zhl/OptixRenderer/src/sutil/tinyxml/tinystr.cpp" "/home/zhl/OptixRenderer/src/sutil/CMakeFiles/sutil_sdk.dir/tinyxml/tinystr.cpp.o"
+  "/home/zhl/OptixRenderer/src/sutil/tinyxml/tinyxml.cpp" "/home/zhl/OptixRenderer/src/sutil/CMakeFiles/sutil_sdk.dir/tinyxml/tinyxml.cpp.o"
+  "/home/zhl/OptixRenderer/src/sutil/tinyxml/tinyxmlerror.cpp" "/home/zhl/OptixRenderer/src/sutil/CMakeFiles/sutil_sdk.dir/tinyxml/tinyxmlerror.cpp.o"
+  "/home/zhl/OptixRenderer/src/sutil/tinyxml/tinyxmlparser.cpp" "/home/zhl/OptixRenderer/src/sutil/CMakeFiles/sutil_sdk.dir/tinyxml/tinyxmlparser.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/zhl/NVIDIA-OptiX-SDK-6.0.0-linux64/include"
+  "sutil"
+  "optixRenderer/include"
+  "."
+  "/home/zhl/NVIDIA-OptiX-SDK-6.0.0-linux64/include/optixu"
+  "/usr/local/cuda/include"
+  "device_include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
